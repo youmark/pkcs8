415ad27055ff689957d87b4b8d1b486227f6919c
HyeJin Kim <cathy@lokks.io>
2019-12-11 11:00:31 +0900

    # ADD - more oid hash, keylen in pbkdf2 params

--- kdf_pbkdf2.go
+++ kdf_pbkdf2.go
@@ -2,8 +2,10 @@ package pkcs8
 
 import (
 	"crypto"
+	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"errors"
@@ -13,9 +15,15 @@ import (
 )
 
 var (
-	oidPKCS5PBKDF2    = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5, 12}
-	oidHMACWithSHA1   = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 7}
-	oidHMACWithSHA256 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 9}
+	oidPKCS5PBKDF2        = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5, 12}
+	oidHMACWithMD5        = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 5}
+	oidHMACWithSHA1       = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 7}
+	oidHMACWithSHA224     = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 8}
+	oidHMACWithSHA256     = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 9}
+	oidHMACWithSHA384     = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 10}
+	oidHMACWithSHA512     = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 11}
+	oidHMACWithSHA512_224 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 12}
+	oidHMACWithSHA512_256 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 13}
 )
 
 func init() {
@@ -28,8 +36,20 @@ func newHashFromPRF(ai pkix.AlgorithmIdentifier) (func() hash.Hash, error) {
 	switch {
 	case len(ai.Algorithm) == 0 || ai.Algorithm.Equal(oidHMACWithSHA1):
 		return sha1.New, nil
+	case ai.Algorithm.Equal(oidHMACWithMD5):
+		return md5.New, nil
+	case ai.Algorithm.Equal(oidHMACWithSHA224):
+		return sha256.New224, nil
 	case ai.Algorithm.Equal(oidHMACWithSHA256):
 		return sha256.New, nil
+	case ai.Algorithm.Equal(oidHMACWithSHA384):
+		return sha512.New384, nil
+	case ai.Algorithm.Equal(oidHMACWithSHA512):
+		return sha512.New, nil
+	case ai.Algorithm.Equal(oidHMACWithSHA512_224):
+		return sha512.New512_224, nil
+	case ai.Algorithm.Equal(oidHMACWithSHA512_256):
+		return sha512.New512_256, nil
 	default:
 		return nil, errors.New("pkcs8: unsupported hash function")
 	}
@@ -52,6 +72,7 @@ func newPRFParamFromHash(h crypto.Hash) (pkix.AlgorithmIdentifier, error) {
 type pbkdf2Params struct {
 	Salt           []byte
 	IterationCount int
+	KeyLength      int                      `asn1:"optional"`
 	PRF            pkix.AlgorithmIdentifier `asn1:"optional"`
 }
 
@@ -59,6 +80,9 @@ func (p pbkdf2Params) DeriveKey(password []byte, size int) (key []byte, err erro
 	h, err := newHashFromPRF(p.PRF)
 	if err != nil {
 		return nil, err
+	}
+	if p.KeyLength != size && p.KeyLength != 0 {
+		return nil, errors.New("pkcs8: key length missmatch")
 	}
 	return pbkdf2.Key(password, p.Salt, p.IterationCount, size, h), nil
 }
@@ -78,7 +102,7 @@ func (p PBKDF2Opts) DeriveKey(password, salt []byte, size int) (
 	if err != nil {
 		return nil, nil, err
 	}
-	params = pbkdf2Params{salt, p.IterationCount, prfParam}
+	params = pbkdf2Params{salt, p.IterationCount, size, prfParam}
 	return key, params, nil
 }
 
